80c4b01dd7f1dc0e9e96117f0087c3ca76604ea2
Marcelly Guimarães <souzaguimaraesarcelly@gmail.com>
2025-02-10 17:12:14 -0300

    validação

---
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ContainerComponent } from '../../componentes/container/container.component';
+import { SeparadorComponent } from '../../componentes/separador/separador.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-formulario-contato',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ContainerComponent,
+    SeparadorComponent,
+    ReactiveFormsModule
+  ],
+  templateUrl: './formulario-contato.component.html',
+  styleUrl: './formulario-contato.component.css'
+})
+export class FormularioContatoComponent {
+
+  contatoForm!: FormGroup;
+
+  constructor() {
+    this.contatoForm = new FormGroup({
+      nome: new FormControl('isis'),
+      telefone: new FormControl('99 999999999'),
+      email: new FormControl('isis@teste'),
+      aniversario: new FormControl(''),
+      redes: new FormControl(''),
+      observacoes: new FormControl('ola mundo')
+    })
+  }
+
+  salvarContato() {
+    console.log(this.contatoForm.value)
+  }
+
+  cancelar() {
+    console.log("Submissão cancelada")
+  }
+}
