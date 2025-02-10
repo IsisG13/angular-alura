4f87f1c34e776a9ff5252bea15559b0563e810fb
Marcelly Guimarães <souzaguimaraesarcelly@gmail.com>
2025-02-10 18:22:14 -0300

    validacoes

--- src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -2,7 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { SeparadorComponent } from '../../componentes/separador/separador.component';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-contato',
@@ -11,31 +16,32 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     ContainerComponent,
     SeparadorComponent,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   templateUrl: './formulario-contato.component.html',
-  styleUrl: './formulario-contato.component.css'
+  styleUrl: './formulario-contato.component.css',
 })
 export class FormularioContatoComponent {
-
   contatoForm!: FormGroup;
 
   constructor() {
     this.contatoForm = new FormGroup({
-      nome: new FormControl('isis'),
-      telefone: new FormControl('99 999999999'),
-      email: new FormControl('isis@teste'),
+      nome: new FormControl('', Validators.required),
+      telefone: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
       aniversario: new FormControl(''),
       redes: new FormControl(''),
-      observacoes: new FormControl('ola mundo')
-    })
+      observacoes: new FormControl(''),
+    });
   }
 
   salvarContato() {
-    console.log(this.contatoForm.value)
-  }
+    if(this.contatoForm.valid) {
+        console.log(this.contatoForm.value);
+    }
+}
 
   cancelar() {
-    console.log("Submissão cancelada")
+    console.log('Submissão cancelada');
   }
 }
